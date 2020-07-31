e80d65792d8719b6ab709d0d7ebb0915cb6d0f60
Mathew Chanda <mvchanda@ncsu.edu>
2020-07-31 17:49:31 -0400

    Replaced navbar with old navbar

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -17,27 +17,20 @@ function NavBar(props){
             imageStyle:{
                 width: 45, 
                 height: 45,
-                margin: 6,
-                marginLeft: 100
+                margin: 6
             }, 
             centerStyle:{
                 flex: 1,
                 flexDirection: "row",
-                //justifyContent:"center", 
-                //alignItems: "center",
-                //width: "100%",
-                backgroundColor: "blue"
+                justifyContent:"center", 
+                alignItems: "center"
             },
             textStyle:{
                 color:"white",
                 fontSize: 40, 
                 //fontFamily: "Poppins",
                 fontWeight:"bold",
-                //marginLeft : 10
-                //marginRight: 200,
-                flex: 1,
-                textAlign: "center",
-                marginRight: 80
+                marginLeft : 10
             }, 
             navBarStyle:{
                 marginTop:15,
